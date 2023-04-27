9be4cd80ab0079015590da8a3dd5e42311829082
D3Ti <62066592+TheD3Ti@users.noreply.github.com>
2023-04-27 08:24:03 -0700

    Update send.php

--- send.php
+++ send.php
@@ -3,9 +3,9 @@ $servername = "localhost";
 $database = "dsd";
 $username = "admin";
 $password = "admin";
-// Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
+
 if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
